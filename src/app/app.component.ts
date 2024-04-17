bdedeb7906c489493a9a12fa06ed018cad99a3b7
ozcanbayram <ozzcanbayram@gmail.com>
2024-04-17 17:40:41 +0300

    Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ToDo-Angular';
+  user = 'Ozcan';
+  items = [
+    {description: "Kahvaltı", action: "No"},
+    {description: "Ders Çalışma", action: "No"},
+    {description: "Spor", action: "No"},
+    {description: "Sinema", action: "No"}
+  ];
 }
